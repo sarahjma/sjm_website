95aef0463f3c509516f70127e4282f427c0e9113
Sarah Ma <sarahjma4@gmail.com>
2020-02-10 16:13:10 -0500

    started landing page with video and title

---
+++ myscript.js
@@ -0,0 +1,16 @@
+var myvid = document.getElementById('landingvideo');
+
+myvid.addEventListener('ended', function(e) {
+  // get the active source and the next video source.
+  // I set it so if there's no next, it loops to the first one
+  var activesource = document.querySelector("#landingvideo source.active");
+  var nextsource = document.querySelector("#landingvideo source.active + source") || document.querySelector("#landingvideo source:first-child");
+
+  // deactivate current source, and activate next one
+  activesource.className = "";
+  nextsource.className = "active";
+
+  // update the video source and play
+  myvid.src = nextsource.src;
+  myvid.play();
+});
